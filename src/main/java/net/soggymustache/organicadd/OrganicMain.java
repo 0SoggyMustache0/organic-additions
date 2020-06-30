df301c680daf65e7953d42ebd03dc9c067af590c
George Kazanjian <soggymustacheofficialmods@gmail.com>
2020-06-30 16:45:23 -0400

    Change build number for release

--- src/main/java/net/soggymustache/organicadd/OrganicMain.java
+++ src/main/java/net/soggymustache/organicadd/OrganicMain.java
@@ -28,7 +28,7 @@ public class OrganicMain {
 
 	public static final String MOD_ID = "organicadd";
 	public static final String NAME = "Organic Additions";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.2";
 
 	@Mod.EventBusSubscriber(modid = MOD_ID)
     public static class Handlers{
